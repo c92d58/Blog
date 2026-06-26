590cf8e1794d5aad909b1ff3240e09fc43b008fc
c92d58 <x@wahsun.org>
2026-06-26 09:32:33 +0800

    deploy: fix canonical URL + base path

--- src/env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-
-interface ImportMetaEnv {
-  readonly PUBLIC_GOOGLE_SITE_VERIFICATION?: string;
-  readonly PUBLIC_PLAUSIBLE_DOMAIN?: string;
-  readonly PUBLIC_FATHOM_SITE_ID?: string;
-}
